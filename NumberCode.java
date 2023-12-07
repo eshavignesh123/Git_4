0e3794e4098b6e93c7474c539b49158b3de1f0ed
Esha Vigneswaran <30001335@sbstudents.org>
2023-12-06 20:21:02 -0500

    Second Commit

--- NumberCode.java
+++ NumberCode.java
@@ -1,3 +1,11 @@
 public class NumberCode {
+    public static void main(String[] args) {
+        int num = 0;
+        for (int i = 0; i < 5; i++) {
+            num = num * 10 + i;
+            System.out.println(num);
+        }
+    }
+
 
 }
